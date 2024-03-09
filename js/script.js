f431ae7caf96c779dc966c3de5aa667c55e04ac2
David <phormula4real@gmail.com>
2024-03-09 01:45:31 +0000

    make visually appealing

--- js/script.js
+++ js/script.js
@@ -3,36 +3,24 @@ const products = [
     {
         name: "Sobolo",
         image: "images/sobolo.jpg",
-        formUrl: "https://forms.gle/D6psfqHATPpLs5Vp8"
+        formUrl: "https://forms.gle/wckrQeMaFoP5e2E86"
     },
     {
         name: "Pineapple Juice",
         image: "images/pineapple-juice.jpg",
-        formUrl: "https://forms.gle/D6psfqHATPpLs5Vp8"
+        formUrl: "https://forms.gle/wckrQeMaFoP5e2E86"
     }
     ,
     {
         name: "Lemon Juice",
         image: "images/lemon-juice.jpg",
-        formUrl: "https://forms.gle/D6psfqHATPpLs5Vp8"
+        formUrl: "https://forms.gle/wckrQeMaFoP5e2E86"
     }
 ];
 
 // Function to open Google Form when the "Order" button is clicked
 function openGoogleForm(formUrl) {
     window.open(formUrl, '_blank');
-}
-
-// Function to share product on Facebook
-function shareOnFacebook(productName, productUrl) {
-    const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(productUrl)}&quote=${encodeURIComponent(productName)}`;
-    window.open(url, '_blank');
-}
-
-// Function to share product on Twitter
-function shareOnTwitter(productName, productUrl) {
-    const url = `https://twitter.com/intent/tweet?url=${encodeURIComponent(productUrl)}&text=${encodeURIComponent(productName)}`;
-    window.open(url, '_blank');
 }
 
 // Function to display products
@@ -48,8 +36,6 @@ function displayProducts() {
                 <img src="${product.image}" alt="${product.name}">
                 <h3>${product.name}</h3>
                 <button onclick="openGoogleForm('${product.formUrl}')">Order</button>
-                <button onclick="shareOnFacebook('${product.name}', '${window.location.href}')">Share on Facebook</button>
-                <button onclick="shareOnTwitter('${product.name}', '${window.location.href}')">Share on Twitter</button>
             </div>
         `;
 
